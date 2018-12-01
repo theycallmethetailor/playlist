95921e91bb6c823ccd3d8bcfb31f854533bbe236
Amanda <aeverett1317@gmail.com>
2018-11-30 17:38:14 -0700

    added some styling begin working on stretch page

--- script2.js
+++ script2.js
@@ -1,68 +1,73 @@
 document.addEventListener("DOMContentLoaded", function() {
-  let newAlbumn
-  let newAlbumnImage
-  let newTrack
-  const albumContaier = document.querySelector('.album-container')
-  const albumnInfo = document.querySelector('.albumn-info')
+  let newAlbumn;
+  let newAlbumnImage;
+  let newTrack;
+  const albumContaier = document.querySelector('.album-container');
+  const albumnInfo = document.querySelector('.albumn-info');
+  const clearButton = document.querySelector('#clear');
+  const submitBin = document.querySelector('#submitBin');
+  const trackListParent = document.createElement("ol")
+  let counter = 0;
+
+  trackListParent.className = "track-list"
+  albumnInfo.appendChild(trackListParent)
 
 
   //GET REQUEST FOR ALBUMN INFO IBJECT
   fetch("https://lit-fortress-6467.herokuapp.com/object")
     .then(function(response) {
+      console.log(response);
       return response.json();
     })
     .then(function(jSONalbums) {
 
+      //THIS LOOP ADDS THE TRACK INFO FOR EACH CLICKED ALBUMN TO THE PAGE AND PUSHES IT TO THE ARRAY LIST TO BE POSTED LATER
       for (var i = 0; i < jSONalbums.results.length; i++) {
         var trackList = [];
-        let artist = jSONalbums.results[i].artist
-        let title = jSONalbums.results[i].title
+        let artist = jSONalbums.results[i].artist;
+        let title = jSONalbums.results[i].title;
         newAlbumn = document.createElement('div');
         newAlbumn.id = `album${jSONalbums.results[i].id}`;
-
-        newAlbumnImage = document.createElement('img')
+        newAlbumnImage = document.createElement('img');
         newAlbumnImage.src = `images/${jSONalbums.results[i].cover_art}`;
         albumContaier.appendChild(newAlbumn);
         newAlbumn.appendChild(newAlbumnImage);
 
+        //THIS ADDS AN EVENT LISTENER TO EACH ALBUMN COVER ELEMENT THAT APPENDS THE TRACKLIST TO THE TRACKLIST DIV ON THE PAGE
+
         newAlbumnImage.addEventListener("click", function(e) {
-          newTrack = document.createElement('span');
-          newTrack.className = "m-2 track d-block"
-          newTrack.style = ""
+          counter ++
 
-          newTrack.textContent = `${artist}: ${title}`
-
-          albumnInfo.appendChild(newTrack)
-          trackList.push(newTrack.textContent)
+          newTrack = document.createElement('li');
+          newTrack.className = "m-2 track d-block";
+          newTrack.textContent =   `${artist}: ${title}`;
+          trackListParent.appendChild(newTrack);
+          trackList.push(`${artist}: ${title}`);
           console.log(trackList);
-
         })
       }
 
+      //THIS EVENT LISTENER REMOVES THE CLICKED ON TRACK FROM BOTH THE PAGE AND THE TRACKLIST ARRAY BEING POSTED
       albumnInfo.addEventListener("click", function(e) {
-        if (event.target.tagName.toLowerCase() === 'span') {
-          clickedTrack = event.target
-          parent = clickedTrack.parentElement
-          parent.removeChild(clickedTrack)
-          trackList.splice(trackList.indexOf(clickedTrack.textContent), 1)
+        if (event.target.tagName.toLowerCase() === 'li') {
+          clickedTrack = event.target;
+          parent = clickedTrack.parentElement;
+          parent.removeChild(clickedTrack);
+          trackList.splice(trackList.indexOf(clickedTrack.textContent), 1);
+          console.log(trackList);
         }
       })
 
-
-      const clearButton = document.querySelector('#clear')
-
+      //THIS CLEARS BOTH THE LIST ON THE PAGE AND THE TRACKLIST TO BE POSTED TO THE PAGE
       clearButton.addEventListener("click", function(e) {
-        e.preventDefault()
-        albumnInfo.innerHTML = ""
+        e.preventDefault();
+        albumnInfo.innerHTML = "";
         trackList = [];
       })
 
-      // console.log(tracklist);
 
 
-
-      const submitBin = document.querySelector('#submitBin');
-
+      //THIS EVENT LISTENER CREATES A POST REQUEST FOR WHEN THE SUBMIT BIN BUTTON IS CLICKED, POSTING THE TRACKLIST ARRAY
       submitBin.addEventListener("click", function(e) {
         e.preventDefault();
         let axios = {
@@ -79,23 +84,13 @@ document.addEventListener("DOMContentLoaded", function() {
                 .then(text => {
                   console.log(text, body)
                 })
-                // .then(function(data) { console.log(data);})
             }
         }
         var url = "https://lit-fortress-6467.herokuapp.com/post";
-
-        axios.post(url, {data: trackList})
-            // .then(data => console.log('data', data))
+        axios.post(url, {data: trackList});
       })
-
-        // function (thisVariableisGoingtoBetheThingThatWeWantToMultuplyBy2) {
-        //   thisVariableisGoingtoBetheThingThatWeWantToMultuplyBy2 * 2
-        // }
-        // var thisIsWhatWeAreMultuplyingByITself = "this is the string that we are duplicating"
-        // var thisIsTheVariableThatWeAreGoingToSquare = thisIsWhatWeAreMultuplyingByITself * thisIsWhatWeAreMultuplyingByITself
-        //
-        // console.log(thisIsTheVariableThatWeAreGoingToSquare);
-
     })
+
+
 
 }) //END OF CONTENT LOAD EVENT LISTENER FUNCTION
